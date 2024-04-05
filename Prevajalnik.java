711f39c7ff60a29e76b84be0a9a1aad93c366e13
matjazi-k <mk92169@student.uni-lj.si>
2024-04-05 22:15:54 +0200

    Now supports multiple languages kinda

--- Prevajalnik.java
+++ Prevajalnik.java
@@ -1,55 +1,66 @@
 import java.io.*;
 import java.util.*;
+import java.lang.*;
 
 public class Prevajalnik {
-	static String words_file = "translate.csv";
 	static HashMap<String,String> wordsAndTranslations = new HashMap<String, String>();
-	static String[] possiblePrefixes = new String[]{" ", "\\)", "\\}","\t","\n"};
-	static String[] possibleSufixes = new String[]{" ", "\\(", "\\{"};
+	static String[] possiblePrefixes = new String[]{" ", ".", "#", ")", "}","\t","\n"};
+	static String[] possibleSufixes = new String[]{" ", "(", "{"};
 	
 	public static void main(String[] args) {
-		try {
-			String imeDat = args[0];
-			String prevedeno = "";
-		
-			genTranslationCombinations();
-			
-			try {
-				BufferedReader bralec = new BufferedReader(new FileReader(imeDat + ".slojava"));
-			
-				prevedeno = prevediDat(bralec);
-				System.out.println(prevedeno);
-				bralec.close();
-			} catch (FileNotFoundException e) {
-				System.out.println("Datoteke " + imeDat + ".slojava ni bilo mogoče najti.\nPreverite, če ste pravilno vnesli ime datoteke.");
-			} catch (IOException e) {
-				System.out.println("Datoteke ni mogoče zapreti za branje.");
-			}
-			
-			try {
-				BufferedWriter pisatelj = new BufferedWriter(new FileWriter(imeDat + ".java"));
-				pisatelj.write(prevedeno, 0, prevedeno.length());
-				pisatelj.close();
-				System.out.println("Done!");
-			} catch (IOException e) {
-				System.out.println("Datoteke " + imeDat + ".slojava ni bilo mogoče shraniti.");
-			}
-		} catch (Exception e) {
+		if (args.length < 2) {
 			System.out.println("Niste podali imena datoteke!");
 		}
+		
+		String imeDat = args[0];
+		String plang = args[1];
+		String lang = "slo";
+		String prevedeno = "";
+		
+		genTranslationCombinations(lang, plang);
+		
+		try {
+			BufferedReader bralec = new BufferedReader(new FileReader(imeDat + "." + lang + plang));
+			
+			prevedeno = prevediDat(bralec);
+			System.out.println(prevedeno);
+			bralec.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Datoteke " + imeDat + "." + lang + plang + " ni bilo mogoče najti.\nPreverite, če ste pravilno vnesli ime datoteke.");
+		} catch (IOException e) {
+			System.out.println("Datoteke ni mogoče zapreti za branje.");
+		}
+			
+		try {
+			BufferedWriter pisatelj = new BufferedWriter(new FileWriter(imeDat + "." + plang));
+			pisatelj.write(prevedeno, 0, prevedeno.length());
+			pisatelj.close();
+			System.out.println("Done!");
+		} catch (IOException e) {
+			System.out.println("Datoteke " + imeDat + "." + lang + plang + " ni bilo mogoče shraniti.");
+		}
+		
+		String command = "javac " + imeDat + "." + plang;
+		ProcessBuilder pB = new ProcessBuilder();
+		pB.command("cmd", "/c", command);
+		try {
+			Process p = pB.start();
+		}
+		catch (Exception e) {}
 	}
 	
-	public static void genTranslationCombinations() {
+	public static void genTranslationCombinations(String lang, String plang) {
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader(words_file));
+			File file = new File("./languages/"+plang+"/"+lang+".csv");
+			BufferedReader reader = new BufferedReader(new FileReader(file));
 			String line;
 			
 			while ((line = reader.readLine()) != null) {
 				String[] wandt = line.split(";");
 				for (int i = 0; i < possiblePrefixes.length; i++) {
 					for (int j = 0; j < possibleSufixes.length; j++) {
-						String word = possiblePrefixes[i]+wandt[1]+possibleSufixes[j];
-						String transaltion = possiblePrefixes[i]+wandt[0]+possibleSufixes[j];
+						String word = possiblePrefixes[i]+wandt[0]+possibleSufixes[j];
+						String transaltion = possiblePrefixes[i]+wandt[1]+possibleSufixes[j];
 						wordsAndTranslations.put(word, transaltion);
 					}
 				}
@@ -67,13 +78,16 @@ public class Prevajalnik {
 		StringBuilder content = new StringBuilder();
 		String line;
 		String translated = "";
+		
 		try {
 			while ((line = reader.readLine()) != null) {
+				line = " "+line;
 				for (String word : wordsAndTranslations.keySet()) {
 					if (line.contains(word)) {
-						line = line.replaceAll(word, wordsAndTranslations.get(word));
+						line = line.replace(word, wordsAndTranslations.get(word));
 					}
 				}
+				line = line.substring(1,line.length());
 				content.append(line);
 				content.append(System.lineSeparator());
 			}
